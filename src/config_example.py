7c378ad43880f73cb7680d4627479d25c9920eca
silbusch <silja.buschko@t-online.de>
2026-05-12 13:48:48 +0200

    Added

---
+++ src/config_example.py
@@ -0,0 +1,4 @@
+# Copy this file as "config.py" and adjust the paths to suit your system.
+DATA_RAW = r"PATH\TO\EXTERNAL\SSD_OR\RAW_DATA_FOLDER"
+DATA_PROCESSED = r"PATH\TO\PROJECT\data\processed"
+DATA_OUTPUT = r"PATH\TO\PROJECT\data\outputs"
